214077c7dd97c019cba71921bbbc53057d5a614d
pperpez <papelo93@gmail.com>
2017-06-21 09:59:03 +0200

    Create agregar_pelicula_lista.php

---
+++ agregar_pelicula_lista.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'Peliculas.php';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $body = json_decode(file_get_contents("php://input"), true);
+    $retorno = Peliculas::insertPeliculaLista(
+        $body['idLista'],
+        $body['idPelicula']);
+    if ($retorno) {
+        $json_string = json_encode(array("estado" => 1,"mensaje" => "Creacion correcta"));
+		echo $json_string;
+    } else {
+        $json_string = json_encode(array("estado" => 2,"mensaje" => "No se creo el registro"));
+		echo $json_string;
+    }
+}
+?>
